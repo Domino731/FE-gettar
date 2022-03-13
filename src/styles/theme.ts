7cf40db08c704dfff9b0305111a73edecfcdf91f
Domino731 <d.orzechowski18@gmail.com>
2022-03-13 14:00:49 +0100

    styles - theme object

---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+/** app theme */
+export const theme = {
+    black: "#272932",
+    bloodRed: "#710000",
+    yellow: "#FDF500",
+    keppel: "#1AC5B0",
+    purple: "#9370DB",
+    frostbite: "#E455AE",
+    steelPink: "#CB1DCD",
+    paleSilver: "#D1C5C0"
+}
